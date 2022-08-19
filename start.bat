f16e7ce760ece5774621fbef431265b0cbd217c5
Unai Iglesias <unaiiglesiasrodriguez@gmail.com>
2022-08-19 12:39:33 +0200

    Init

---
+++ start.bat
@@ -0,0 +1,2 @@
+java -Xmx8G -Xms8G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=0 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -XX:+UseG1GC -jar forge-1.16.5-36.2.39.jar nogui
+pause
